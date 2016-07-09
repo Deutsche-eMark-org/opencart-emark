5713796277158dd3387828c5dd2d0d909eb26cee
Lukas <xBlackEye@users.noreply.github.com>
2016-07-09 21:11:14 +0200

    Update bitcoin.php

--- upload/admin/language/english/payment/bitcoin.php
+++ upload/admin/language/english/payment/bitcoin.php
@@ -14,15 +14,15 @@
  */
 
 // Heading
-$_['heading_title']         = 'BitCoin';
+$_['heading_title']         = 'Deutsche eMark';
 
 // Text
 $_['text_payment']          = 'Payment';
-$_['text_success']          = 'Success: You have modified BitCoin details!';
-$_['text_edit']             = 'Edit BitCoin';
+$_['text_success']          = 'Success: You have modified eMark details!';
+$_['text_edit']             = 'Edit eMark';
 $_['text_google_api']       = 'Google API';
-$_['text_bitcoin']          = '<a href="https://bitcoin.org" target="_blank"><img src="view/image/payment/bitcoin.png" alt="BitCoin" style="border: 1px solid #EEEEEE;" /></a>';
-$_['text_copyright']        = '<p><a href="https://github.com/shaman/opencart-bitcoin" target="_blank">BitCoin Payment for OpenCart</a></p><p>Version: 0.1.1</a></p><p>Donate BTC: <a href="bitcoin:1HdK2ppceaMvJSZqwEnQcpVaSe2xX3HYEa?label=BitCoin%20for%20OpenCart%20Donate">1HdK2ppceaMvJSZqwEnQcpVaSe2xX3HYEa</a></p><p><a href="https://bitsybay.com" target="_blank">BitsyBay &ndash; Templates and Extensions for OpenCart with BitCoin</a></p>';
+$_['text_bitcoin']          = '<a href="http://deutsche-emark.de/" target="_blank"><img src="view/image/payment/emark.png" alt="Deutsche eMark" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_copyright']        = '';
 
 
 // Entry
@@ -41,14 +41,14 @@ $_['entry_currency']        = 'BitCoin Currency';
 
 // Help
 $_['help_total']            = 'The checkout total the order must reach before this payment method becomes active.';
-$_['help_user']             = 'Bitcoin RPC Username';
-$_['help_password']         = 'Bitcoin RPC Password';
-$_['help_host']             = 'Bitcoin RPC Host, localhost by default';
-$_['help_port']             = 'Bitcoin RPC Port, 8332 by default';
-$_['help_path']             = 'Bitcoin RPC Path, empty by default';
-$_['help_qr']               = 'BitCoin Address will be formatted as additional QR Code';
-$_['help_currency']         = 'Create, activate and change your BitCoin Currency';
+$_['help_user']             = 'eMark RPC Username';
+$_['help_password']         = 'eMark RPC Password';
+$_['help_host']             = 'eMark RPC Host, localhost by default';
+$_['help_port']             = 'eMark RPC Port, 8332 by default';
+$_['help_path']             = 'eMark RPC Path, empty by default';
+$_['help_qr']               = 'eMark Address will be formatted as additional QR Code';
+$_['help_currency']         = 'Create, activate and change your eMark Currency';
 
 // Error
-$_['error_permission']      = 'Warning: You do not have permission to modify payment BitCoin!';
-$_['error_response']        = 'Warning: Could not connect to BitCoin via RPC! Response: %s';
+$_['error_permission']      = 'Warning: You do not have permission to modify payment eMark!';
+$_['error_response']        = 'Warning: Could not connect to eMark via RPC! Response: %s';
