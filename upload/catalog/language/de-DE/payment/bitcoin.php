30e9d8ee3812293ddb4f6c6914466c7787622d47
Lukas <xBlackEye@users.noreply.github.com>
2016-07-09 21:14:38 +0200

    Create bitcoin.php

---
+++ upload/catalog/language/de-DE/payment/bitcoin.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title']           = 'Deutsche eMark';
+$_['text_instruction']     = 'Zahlungsanweisungen';
+$_['text_description']     = 'Bitte sende %s an die folgende DEM Adresse:';
+$_['text_bitcoin_address'] = 'Payment Address: %s';
